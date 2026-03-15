3d18f0f65d14af1f31303c27b92f36c1fe717fdd
prabhguron <prabhten@gmail.com>
2026-03-14 19:55:24 -0700

    a bunch of styling

--- bloccord/src/Ui/EventNodeComponent.tsx
+++ bloccord/src/Ui/EventNodeComponent.tsx
@@ -7,16 +7,25 @@ export function EventNodeComponent({ data, id }: NodeProps) {
   const [keyword, setKeyword] = useState('')
   const { updateNodeData } = useReactFlow()
   return (
-    <div>
+    <div style={{
+  background: '#EAF3DE',
+  border: '1px solid #97C459',
+  borderRadius: '8px',
+  padding: '12px 16px',
+  minWidth: '180px'
+}}>
 
-      <p>Data Event Please type your keyword command</p>
+      <p style={{ fontSize: '12px', color: '#3B6D11', margin: '0 0 8px', fontWeight: 500 }}>
+        Event trigger
+        Data Event Please type your keyword command
+        </p>
       <input
         value={keyword}
         onChange={(e) => {
           setKeyword(e.target.value)
           updateNodeData(id, { keyword: e.target.value })
         }}
-        placeholder="Type command e.g. baby no /"
+        placeholder="Type command e.g. Hi"
       />
 
 
